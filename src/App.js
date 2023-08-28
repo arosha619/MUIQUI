a22f7ab961625b0cad6c7c5d6bea101d1c1eb7b6
arosha619 <90087992+arosha619@users.noreply.github.com>
2023-08-28 22:40:19 +0530

    remove comments

--- src/App.js
+++ src/App.js
@@ -9,36 +9,16 @@ import MyBucket from "./components/MyBucket/MyBucket";
 import Login from "./components/Login/Login";
 import SignUp from "./components/SignUp/SignUp";
 function App() {
-  // const [authenticated, setAuthenticated] = useState(false);
   return (
-    // <Router>
-
-    // {authenticated ?
-    //    <SideBar>
-    //     <Routes>
-    //       <Route path="/home" element={<Dashboard />} />
-    //       <Route path="/user-list" element={<UserList />} />
-    //       <Route path="/my-buckets" element={<MyBucket />} />
-    //       <Route path="/settings" element={<Setting />} />
-
-    //     </Routes>
-    //     </SideBar>
-
-    //   :(<Routes>
-    //   <Route path="/" element={<Login setAuthenticated={setAuthenticated}/>} />
-    //   <Route path="/sign-up" element={<SignUp/>} />
-    //   </Routes>)}
-    // </Router>
-    // <SideBar>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
-        {/* <Route path="/home" element={<Dashboard />} /> */}
+        <Route path="/home" element={<Dashboard />} />
         <Route path="/user-list" element={<UserList />} />
         <Route path="/my-buckets" element={<MyBucket />} />
         <Route path="/settings" element={<Setting />} />
       </Routes>
-    // </SideBar>
+    
   );
 }
 
